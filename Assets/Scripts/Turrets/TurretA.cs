45804ee5859d474085a312c7df74425f1ccca5f2
DeceasedWolf <saltcoffee.mathis@gmail.com>
2022-08-11 14:15:12 -0400

    Started the collision on top of the turret

--- Assets/Scripts/Turrets/TurretA.cs
+++ Assets/Scripts/Turrets/TurretA.cs
@@ -1,32 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TurretA : Turrets
 {
-    [SerializeField] private int shootCount;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //test he he he eh can you see this?
-    }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnCollisionEnter2D(Collision2D col)
     {
-        if (isShooting)
+        
+        if (col.transform.position.y > transform.position.y + 60)
         {
-            CheckFireAmount();
+            if (col.gameObject.TryGetComponent(out Player ply))
+            {
+                ply.StartCoroutine(ply.SetTempSpeed(5, 40));
+            }
+            
         }
+        
+        base.OnCollisionEnter2D(col);
     }
-
-    private void CheckFireAmount()
-    {
-        if (shootCount++ >= 8)
-        {
-            stopShoot -= 0.1f;
-
-            shootCount = 0;
-        }
-    }
+ 
 }
