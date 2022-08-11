165a331af5b54c57529c245d08536900873f57e3
DeceasedWolf <saltcoffee.mathis@gmail.com>
2022-08-11 14:34:06 -0400

    finished speed boost from Turret A
    yall are getting carried hard

--- Assets/Scripts/Turrets/TurretA.cs
+++ Assets/Scripts/Turrets/TurretA.cs
@@ -8,12 +8,12 @@ public class TurretA : Turrets
 
     protected override void OnCollisionEnter2D(Collision2D col)
     {
-        
-        if (col.transform.position.y > transform.position.y + 60)
+        if (col.transform.position.y > transform.position.y)
         {
             if (col.gameObject.TryGetComponent(out Player ply))
             {
-                ply.StartCoroutine(ply.SetTempSpeed(5, ply.moveSpeed * 2));
+                ply.StartCoroutine(ply.SetTempSpeed(5, ply.moveSpeed * 100));
+                
             }
             
         }
